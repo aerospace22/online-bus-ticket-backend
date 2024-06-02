6d2d0cc31ffcf1e4c6ccb6cb53fe53e5e9dc9683
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2024-06-03 07:59:00 +0800

    updated

--- src/modules/bus-tickets/bus-tickets.module.ts
+++ src/modules/bus-tickets/bus-tickets.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaService, PaymentsService } from '@/services';
+import { BusRoutesService } from '@/modules/bus-routes/bus-routes.service';
 import { BusTicketsController } from './bus-tickets.controller';
 import { BusTicketsService } from './bus-tickets.service';
 
 @Module({
   controllers: [BusTicketsController],
-  providers: [BusTicketsService, PrismaService, PaymentsService],
+  providers: [BusTicketsService, BusRoutesService, PrismaService, PaymentsService],
 })
 export class BusTicketsModule {}
